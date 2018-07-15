815641dfe119da2dc9b80b3731750ffe4dcfc989
Azhar Shah <azharshahstl@yahoo.com>
2018-07-15 18:21:27 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+
+def line(katz_deli) 
+  if katz_deli == []
+    puts "The line is currently empty."
+  else 
+    katz_deli.map do |name, index|
+      current_line = "#{index + 1}. " + name
+    puts "The line is currently: #{current_line}"
+    end
+  end
+end
+
+def take_a_number(katz_deli, new_customer)
+  if katz_deli == []
+    puts "The line is currently empty."
+  end
+  
+  
+end
+
+def now_serving()
+  if katz_deli == []
+    puts "There is nobody waiting to be served!."
+  end
+  
+  
+end
